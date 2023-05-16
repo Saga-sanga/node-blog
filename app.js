36b1fbc736952c144477f28a4c81b4ffa2e5f5dc
Saga-sanga <vulcan248@gmail.com>
2023-05-16 21:24:04 +0530

    Feat: Add dynamic templates with EJS

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.listen(port);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.get("/", (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ]
+  res.render("index", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create",  { title: "New Blog" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404"});
+});
