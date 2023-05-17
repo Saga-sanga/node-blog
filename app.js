21a12bcade55f60c0c97d217ce60dafb5a62ed3b
Saga-sanga <vulcan248@gmail.com>
2023-05-17 21:10:27 +0530

    Add model and schema data

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { result } = require("lodash");
+
+require("dotenv").config();
+const mongoPassword = process.env.MONGO_PASSWORD;
+
 const app = express();
+
 const port = 3000;
 
-app.listen(port);
+// Mongodb connection string
+const mongoURI = `mongodb+srv://saga_sanga:${mongoPassword}@node-blog.whqmtry.mongodb.net/Sanga-blog-tuts?retryWrites=true&w=majority`;
+mongoose
+  .connect(mongoURI)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+    // Let the server listen for connections only after its connected to MongoDB. Otherwise, it will throw an error.
+    app.listen(port);
+  })
+  .catch((err) => console.log(err));
 
+// Register view engine
 app.set("view engine", "ejs");
 
 // A middleware is any code that runs between a request and a response
@@ -14,26 +32,62 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "A new Blog",
+    snippet: "A new blog that I have created",
+    body: "A lot of text is not necressary for a blog. You can just write a few words and upload it and it will be a blog. Technically speaking. But now the problem is if the users would want to read your blog.",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("6464d74cf50fa7090397fc2a")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// The name we pass to the model is important because it gets pluralised and looks for the exact name in the MongoDB Collections
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
