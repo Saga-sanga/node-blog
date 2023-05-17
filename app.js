d2eccfea195a4c5f3d30737f4253bd8c039d42b8
Saga-sanga <vulcan248@gmail.com>
2023-05-17 12:28:19 +0530

    Add Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
@@ -6,12 +7,23 @@ app.listen(port);
 
 app.set("view engine", "ejs");
 
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ]
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
   res.render("index", { title: "Home", blogs });
 });
 
@@ -20,9 +32,9 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create",  { title: "New Blog" });
+  res.render("create", { title: "New Blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404"});
+  res.status(404).render("404", { title: "404" });
 });
