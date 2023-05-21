68c422af2d864a345e2a56dd61cd1e1be381b19a
Saga-sanga <vulcan248@gmail.com>
2023-05-21 17:28:54 +0530

    Docs: Update readme

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ A small blog built using Node.js and the EJS view engine.
 
 - [EJS](https://ejs.co/)
 - [Express](https://expressjs.com/)
+- [JWT](https://jwt.io/introduction)
+- [Mongoose](https://mongoosejs.com/docs/index.html)
+
+### Articles I found useful
+
+- [https://vegibit.com/mongoose-relationships-tutorial/](https://vegibit.com/mongoose-relationships-tutorial/)
+
+## Features to Add
+1. Add categories and filter for content
+2. Add author and date to blog
+3. Improve style of blog
+4. Add login and users (WIP)
+5. Manage sessions
--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set("view engine", "ejs");
 // Serves all the files in public dir
 app.use(express.static("public"));
 
-// Third party middleware to log req information
+// Third party middleware to log request information
 app.use(morgan("dev"));
 
 // Encode url data to request body
@@ -61,9 +61,6 @@ app.post("/login", (req, res) => {
   console.log(req.body);
 
   // User validation
-  // TODO: Query MongoDB for user and find match, if match found change state to logged in.
-  // Maybe add encryption and crypto validation for password
-  // Can't store password as plaintext in db, it's possibe to do it though
   User.findOne({ email })
     .then((user) => {
       console.log(user);
@@ -88,7 +85,7 @@ app.post("/login", (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({ message: err.message });
     });
 });
 
@@ -136,9 +133,4 @@ app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
 
-// TODO:
-// 1. Add categories and filter for content
-// 2. Add author and date to blog
-// 3. Improve style of blog
-// 4. Add login and users
-// 5. Manage sessions through cookies
+
